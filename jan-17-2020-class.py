0d62db5ede6ef2894dba6fbfd9c073a073a6c988
Stephanie Smith <stephaniesmith12514@gmail.com>
2020-01-16 15:25:07 -0500

    Create jan-17-2020-class.py

---
+++ jan-17-2020-class.py
@@ -0,0 +1,20 @@
+'''
+Shortest Word - https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+'''
+
+# Start coding here!
+
+
+'''
+Tetris Scoring System - https://www.codewars.com/kata/5da9af1142d7910001815d32
+
+
+Calculate the final score of the game using original Nintendo scoring system.
+'''
+
+#Start coding here
